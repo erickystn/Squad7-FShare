971c42ca0765d8888e29b881a241a357bf978af3
gih-costa <gih.costa123@hotmail.com>
2022-04-03 16:01:32 -0300

    feat01/limpeza-dashboard
    - O conteúdo principal foi limpo
    - Colocado logo e nome provisórios

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -8,19 +8,19 @@ export default class Menu extends Component {
         {/* Main Sidebar Container */}
 <aside className="main-sidebar sidebar-dark-primary elevation-4">
   {/* Brand Logo */}
-  <a href="index3.html" className="brand-link">
-    <img src="dist/img/AdminLTELogo.png" alt="AdminLTE Logo" className="brand-image img-circle elevation-3" style={{opacity: '.8'}} />
-    <span className="brand-text font-weight-light">AdminLTE 3</span>
-  </a>
-  {/* Sidebar */}
+  <Link to="/" className="brand-link">
+  <img src="dist/img/logo.png" alt="AdminLTE Logo" className="brand-image img-circle elevation-3" style={{opacity: '.8'}} />
+    <span className="brand-text font-weight-light">Technical Share</span>
+        </Link>
+    {/* Sidebar */}
   <div className="sidebar">
     {/* Sidebar user panel (optional) */}
     <div className="user-panel mt-3 pb-3 mb-3 d-flex">
       <div className="image">
-        <img src="dist/img/user2-160x160.jpg" className="img-circle elevation-2" alt="User Image" />
+        <img src="dist/img/usuario.jpg" className="img-circle elevation-2" alt="User Image" />
       </div>
       <div className="info">
-        <a href="#" className="d-block">Alexander Pierce</a>
+        <a href="#" className="d-block">Usuário Laranja</a>
       </div>
     </div>
     {/* SidebarSearch Form */}
@@ -43,7 +43,8 @@ export default class Menu extends Component {
         <li className="nav-item">
         <Link to="/" className="nav-link active">
         <i className="nav-icon fas fa-tachometer-alt" />
-          Home</Link>
+          Home
+        </Link>
         </li>
         
         
