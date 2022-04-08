2502ee5b54b51683707314cb96fe155ef12343c0
NaySoares <elienay.soares07@gmail.com>
2022-04-08 09:48:35 -0300

    tela de inicio

---
+++ src/pages/Inicio/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Inicio = () => {
+  return (
+    <main>
+      <h1>test</h1>
+    </main>
+  )
+}
+
+export default Inicio;
+
--- src/routes/index.js
+++ src/routes/index.js
@@ -3,14 +3,14 @@ import Dashboard from '../pages/Dashboard'
 import Mentorias from '../pages/Mentorias'
 import Cadastrar from '../pages/Cadastrar'
 import Login from '../pages/Login'
+import Inicio from '../pages/Inicio'
 import Route from './Route'
 
 
 export default function Routes() {
   return (
     <Switch>
-
-      {/* <Route exact path="/" component={TelaInicial} /> */}
+      <Route exact path="/" component={Inicio} />
       <Route exact path="/cadastrar" component={Cadastrar} />
       <Route exact path="/login" component={Login} />
       <Route exact path="/dashboard" component={Dashboard} isPrivate />
