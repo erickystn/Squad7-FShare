d2d1a2b84745f0482d1feb7098b5ba5950564e51
gih-costa <gih.costa123@hotmail.com>
2022-04-09 11:24:35 -0300

    feat07/tela-cadastro-versão-figma
    - Adequação do layout ao design do figma

--- frontend/src/pages/Cadastrar/index.js
+++ frontend/src/pages/Cadastrar/index.js
@@ -6,63 +6,52 @@ export default class Cadastrar extends Component {
 
     return (
       <div>
-        <div className="register-page" >
-          <div className='col-12 col-sm-8 col-md-8'>
-            <div className='card card-solid'>
-              <div className='card-body pb-0'>
-                <div className='row justify-content-center'>
-                  <div className='col-12 col-sm-12 col-md-12 d-flex align-items-stretch flex-column'>
-                    <form>
-                      <div>
-                        <h2>Cadastre-se</h2>
-                      </div>
-                      <div className="row">
-                        <div className='flex-column col-md-3'>
-                          <img src="dist/img/botaofoto.png" className="mt-4" width={130} alt="Logo" />
-                        </div>
-                        <div className='flex-column col-md-9'>
-                          <div className="form-group">
-                            <label htmlFor="name">Seu nome</label><span className='star-red'>*</span>
-                            <div class="input-group mb-3">
-                              <input type="text" id="name" class="form-control input" placeholder="" aria-label="Digite seu nome" aria-describedby="basic-addon2" />
-                            </div>
-                          </div>
-                          <div className="form-group">
-                            <label htmlFor="position">Seu cargo</label><span className='star-red'>*</span>
-                            <div class="input-group mb-3">
-                              <input type="text" id="position" class="form-control input" placeholder="" aria-label="Digite seu cargo" aria-describedby="basic-addon2" />
-                            </div>
-                          </div>
-                        </div>
-                      </div>
-                      <div className="form-group">
-                        <label htmlFor="email">E-mail</label><span className='star-red'>*</span>
-                        <div class="input-group mb-3">
-                          <input type="text" class="form-control input" placeholder="" aria-label="Digite seu email" id="email" aria-describedby="basic-addon2" />
-                          <div class="input-group-append">
-                            <span class="input-group-text" id="basic-addon2">@fcamara.com.br</span>
-                          </div>
-                        </div>
-                      </div>
-                      <div className="form-group">
-                        <label htmlFor="password">Senha</label><span className='star-red'>*</span>
-                        <input type="password" class="form-control input" id="password" placeholder="" />
-                      </div>
-
-                      <div className="form-group">
-                        <label htmlFor="password-confirmation">Confirmar senha</label><span className='star-red'>*</span>
-                        <input type="password" class="form-control input" id="password-confirmation" placeholder="" />
-                      </div>
-
-
-                      <div class="text-center ">
-                        <button type="submit" className="btn btn-signup">ENTRAR</button>
-                      </div>
-                    </form>
+        <div className="register-page box-register" >
+          <div className='col-10 col-sm-10 col-md-10 d-flex align-items-stretch flex-column'>
+            <form>
+              <div>
+                <h2>Cadastre-se</h2>
+              </div>
+              <div className="row">
+                <div className='flex-column col-md-2'>
+                  <img src="dist/img/botaofoto.png" className="mt-4" width={130} alt="Logo" />
+                </div>
+                <div className='flex-column col-md-10'>
+                  <label htmlFor="name" className="label">Seu nome</label><span className='star-red'>*</span>
+                  <div class="input-group mb-3">
+                    <input type="text" id="name" class="form-control input" placeholder="" aria-label="Digite seu nome" aria-describedby="basic-addon2" />
+                  </div>
+                  <label htmlFor="position" className="label">Seu cargo</label><span className='star-red'>*</span>
+                  <div class="input-group mb-3">
+                    <input type="text" id="position" class="form-control input" placeholder="" aria-label="Digite seu cargo" aria-describedby="basic-addon2" />
                   </div>
                 </div>
               </div>
-            </div>
+              <label htmlFor="email" className="label">E-mail</label><span className='star-red'>*</span>
+              <div class="input-group mb-3">
+                <input type="text" class="form-control input" placeholder="" aria-label="Digite seu email" id="email" aria-describedby="basic-addon2" />
+                <div class="input-group-append">
+                  <span class="input-group-text" id="basic-addon2">@fcamara.com.br</span>
+                </div>
+              </div>
+              <div className="form-group">
+                <label htmlFor="password" className="label">Senha</label><span className='star-red'>*</span>
+                <input type="password" class="form-control input" id="password" placeholder="" />
+              </div>
+
+              <div className="form-group">
+                <label htmlFor="password-confirmation" className="label">Confirmar senha</label><span className='star-red'>*</span>
+                <input type="password" class="form-control input" id="password-confirmation" placeholder="" />
+              </div>
+
+
+              <div>
+                <div>
+                  <button type="submit" className="btn-signup">ENTRAR</button>
+                </div>
+
+              </div>
+            </form>
           </div>
         </div>
 
