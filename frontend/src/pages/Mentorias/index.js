09fd8d4bb18a2046ad4e8e5e2bb537e8a985704b
gih-costa <gih.costa123@hotmail.com>
2022-04-10 15:32:35 -0300

    feat09/responsividade-tela-mentor
    - Adequação da página de mentores para telas acima de 720 x 1080

--- frontend/src/pages/Mentorias/index.js
+++ frontend/src/pages/Mentorias/index.js
@@ -10,7 +10,7 @@ export default function Mentorias() {
 
       <section class="content padding-header">
 
-
+        {/* Header */}
         <div>
           <div className='row justify-content-between'>
             <h2>Buscar mentor</h2>
@@ -22,6 +22,7 @@ export default function Mentorias() {
             </Link>
           </div>
 
+          {/* Seacrh bar */}
           <div class="row search-bar">
             <div class="col-12 col-sm-12 col-md-12 d-flex align-items-stretch flex-column">
 
@@ -35,23 +36,26 @@ export default function Mentorias() {
             </div>
           </div>
 
-
-          <div class="col-12 col-sm-12 col-md-12 d-flex row  justify-content-between">
-            <button className='btn btn-stacks col-5 col-md-2 col-sm-6'>NodeJS</button>
-            <button className='btn btn-stacks col-5 col-md-2 col-sm-6'>React</button>
-            <button className='btn btn-stacks col-5 col-md-2 col-sm-6'>CSS</button>
-            <button className='btn btn-stacks col-5 col-md-2 col-sm-6'>MySQL</button>
-            <button className='btn btn-stacks col-5 col-md-2 col-sm-6'>C#</button>
+          {/* Stacks */}
+          <div class="row justify-content-between">
+            <button className='btn btn-stacks'>NodeJS</button>
+            <button className='btn btn-stacks'>React</button>
+            <button className='btn btn-stacks'>CSS</button>
+            <button className='btn btn-stacks'>MySQL</button>
+            <button className='btn btn-stacks'>C#</button>
+            <button className='btn btn-stacks'>Javascript</button>
           </div>
 
-          <div className='row'>
-            <div className='col-12 col-md-6 col-sm-12'>
-              <div class="card shadow-card">
+          {/* Stacks Mentor Cards */}
+
+          <div className='row justify-content-between'>
+            <div className='card-size'>
+              <div class="card card-area">
                 <div class="row">
-                  <div class="col-4 p-3">
+                  <div class="area-img p-3">
                     <img src="dist/img/rihana.png" alt="user-avatar" class="img-circle img-fluid profile-img" />
                   </div>
-                  <div class="col-5 p-3 info-card">
+                  <div class="p-3 info-card">
                     <h2 class="mentor-name"><b>Rihanna Silva</b></h2>
                     <p class="text-muted text-sm">Dev na BoysCry</p>
                     <a href="/" class="btn btn-sm btn-profile">
@@ -59,7 +63,7 @@ export default function Mentorias() {
                     </a>
                   </div>
 
-                  <div class="col-3 d-flex flex-column justify-content-center padding-stacks">
+                  <div class="d-flex flex-column justify-content-center padding-stacks">
                     <div class="chip">
                       Oracle
                     </div>
@@ -75,13 +79,14 @@ export default function Mentorias() {
               </div>
             </div>
 
-            <div className='col-12 col-md-6 col-sm-12'>
+            {/* Infos Mentor Cards */}
+            <div className='card-size'>
               <div class="card shadow-card">
                 <div class="row">
-                  <div class="col-4 p-3">
+                  <div class="area-img p-3">
                     <img src="dist/img/fabio.png" alt="user-avatar" class="img-circle img-fluid profile-img" />
                   </div>
-                  <div class="col-5 p-3 info-card">
+                  <div class="p-3 info-card">
                     <h2 class="mentor-name"><b>Fabio Costa</b></h2>
                     <p class="text-muted text-sm">Dev na FCamara</p>
                     <a href="/" class="btn btn-sm btn-profile">
@@ -89,7 +94,7 @@ export default function Mentorias() {
                     </a>
                   </div>
 
-                  <div class="col-3 d-flex flex-column justify-content-center padding-stacks">
+                  <div class="d-flex flex-column justify-content-center padding-stacks">
                     <div class="chip">
                       NodeJS
                     </div>
@@ -106,12 +111,9 @@ export default function Mentorias() {
             </div>
           </div>
 
-
-
-
         </div>
 
-        <div class="card-footer">
+        {/* <div class="card-footer">
           <nav aria-label="Contacts Page Navigation">
             <ul class="pagination justify-content-center m-0">
               <li class="page-item active"><a class="page-link" href="/">1</a></li>
@@ -124,11 +126,11 @@ export default function Mentorias() {
               <li class="page-item"><a class="page-link" href="/">8</a></li>
             </ul>
           </nav>
-        </div>
+        </div> */}
 
 
 
-      </section>
+      </section >
     </div >
   )
 }
