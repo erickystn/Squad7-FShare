da8931e68fe8a5c507c4907fc156702fb8e9e449
gih-costa <gih.costa123@hotmail.com>
2022-04-11 09:02:20 -0300

    feat10/configuração-pg-perfil

---
+++ frontend/src/pages/Perfil/index.js
@@ -0,0 +1,8 @@
+import React from 'react'
+
+export default function Perfil() {
+  return (
+    <div>Perfil</div>
+  )
+}
+
--- frontend/src/routes/index.js
+++ frontend/src/routes/index.js
@@ -2,7 +2,7 @@ import { Switch } from 'react-router-dom'
 import Dashboard from '../pages/Dashboard'
 import Mentorias from '../pages/Mentorias'
 import Cadastrar from '../pages/Cadastrar'
-import Login from '../pages/Login'
+import Perfil from '../pages/Perfil'
 import Inicio from '../pages/Inicio'
 import Route from './Route'
 
@@ -12,7 +12,7 @@ export default function Routes() {
     <Switch>
       <Route exact path="/" component={Inicio} />
       <Route exact path="/cadastrar" component={Cadastrar} />
-      <Route exact path="/login" component={Login} />
+      <Route exact path="/perfil" component={Perfil} isPrivate />
       <Route exact path="/dashboard" component={Dashboard} isPrivate />
       <Route exact path="/mentorias" component={Mentorias} isPrivate />
     </Switch>
