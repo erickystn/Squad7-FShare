d4f016f8891b4a372a279417c5edc59ddddbb4d3
Elienai <elienay.soares07@gmail.com>
2022-04-12 21:50:38 -0300

    tela de perfil com chakra

--- frontend/src/routes/index.js
+++ frontend/src/routes/index.js
@@ -5,6 +5,7 @@ import Cadastrar from '../pages/Cadastrar'
 import Login from '../pages/Login'
 import Inicio from '../pages/Inicio'
 import SelecionarSkill from '../pages/Cadastrar/selecionarSkill'
+import Perfil from '../pages/Perfil'
 import Route from './Route'
 
 
@@ -15,6 +16,7 @@ export default function Routes() {
       <Route exact path="/cadastrar" component={Cadastrar} />
       <Route exact path="/skills" component={SelecionarSkill} />
       <Route exact path="/login" component={Login} />
+      <Route exact path="/perfil" component={Perfil} isPrivate/>
       <Route exact path="/dashboard" component={Dashboard} isPrivate />
       <Route exact path="/mentorias" component={Mentorias} isPrivate />
     </Switch>
